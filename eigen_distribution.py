c21e31bf026cca130ad85266b0feda30e62389b2
Ludovic Stephan <ludovic.stephan@ens.fr>
2021-02-09 22:13:39 +0100

    Rework eigen_distribution.py

--- eigen_distribution.py
+++ eigen_distribution.py
@@ -1,30 +1,40 @@
 """
-Code for the generation of the histograms of the entries of the eigenvectors.
+    Module `eigen_distribution.py`.
+    Plots the empirical distribution of eigenvectors of random graphs, along with
+    Gaussian approximations.
 
+    Authors: S. Coste and L. Stephan
+
+    References:
+    [1]: Simon Coste and Ludovic Stephan. A simpler spectral approach for clustering in
+    directed networks, 2021. https://arxiv.org/abs/2102.03188
 """
-from os.path import exists
+from os import path
 
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy.sparse.linalg import eigs
-from scipy.stats import norm
 
 from generator import fast_sbm
+from utils import compute_rzero
 
 n = 1000
 n_tries = 5
 
 
-def model_params(F, p):
+def gaussian_params(F, p):
     """
+    Computes the mean and variances of each informative eigenvector in a DSBM for each
+    cluster. Details of the computations are in [1].
+
     Input:
         - F : connectivity matrix
         - p : proportion of vertices in each cluster
 
     Output:
-        - r0: number of informative eigenvalues
-        - means: theoretical means for gaussian mixture
-        - variances : theoretical variances
+        - means: array of shape (r, r0) such that means[i, j] is the (theoretical) mean
+        of the j-th eigenvector on the i-th cluster.
+        - variances : idem for variances
     """
     r = p.size
     mod = F.dot(np.diag(p))
@@ -55,65 +65,58 @@ def model_params(F, p):
         means[:, i] = eigvecs[:, i] / np.sqrt(normsq_ui)
         variances[:, i] = (defects - eigvsq[:, i]) / normsq_ui
 
-    return r0, means, variances
+    return means, variances
 
 
-def gen_data(F, p, r0=None, n=1000, n_tries=20, datafile=None):
+def gen_data(F, p, n_nodes=1000, n_tries=20, datafile=None):
     """
-    Required:
+    Input:
         - F : connectivity matrix
         - p : proportion of vertices in each cluster
-
-    Optional:
-        - r0 : number of informative eigenvalues. Default is p.size, i.e. all eigenvalues are considered.
-        - n : size of the generated graph. Default is 1000
-        - n_tries : number of graphs generated. Default is 20.
-        - datafile : filename to extract information from/save to.
+        - n_nodes: size of the generated graph. Default is 1000
+        - n_tries: number of graphs generated. Default is 20.
+        - datafile: if provided, the generated data is saved to disk.
 
     Output:
         - tot: (n * n_tries, r_0) matrix containing the aggregated eigenvectors.
         Each eigenvectors has norm sqrt(n).
     """
 
-    if r0 is None:
-        r0 = p.size
+    mod = F.dot(np.diag(p))
+    eigvals = np.linalg.eigvals(mod)
+    _, r0 = compute_rzero(eigvals)
+    tot = np.zeros((n_nodes * n_tries, r0))
 
-    if datafile is not None and exists(datafile):
-        tot = np.genfromtxt(datafile)
+    for i in range(n_tries):
+        G, _ = fast_sbm(n_nodes, F, left_proportions=p, right_proportions=p)
+        vals, v = eigs(G, r0, which="LM", return_eigenvectors=True)
 
-        # if tot.shape != (n * n_tries, r0):
-        # raise ValueError(f"Datafile does not contain a ({n * n_tries}, {r0}) matrix.")
+        # Take eigenvectors aligned with the theoretical ones
+        for k in range(r0):
+            if v[0, k] < 0:
+                v[:, k] = -v[:, k]
+        tot[i * n_nodes : (i + 1) * n_nodes, :] = np.sqrt(n_nodes) * v.real
 
-    else:
-
-        for i in range(n_tries):
-            G = fast_sbm(n, F, left_proportions=p, right_proportions=p)
-            if i == 0:
-                actual_size = G.shape[0]
-                tot = np.zeros((actual_size * n_tries, r0))
-            vals, v = eigs(G, r0, which="LM", return_eigenvectors=True)
-            for k in range(r0):
-                if v[0, k] < 0:
-                    v[:, k] = -v[:, k]
-            tot[i * actual_size : (i + 1) * actual_size, :] = np.sqrt(n) * v.real
-
-        if datafile is not None:
-            np.savetxt(datafile, tot)
+    if datafile is not None:
+        np.savetxt(datafile, tot)
 
     return tot
 
 
-F = np.array([[48, 6], [12, 24]])
-p = np.array((1 / 3, 2 / 3))
+def plot_gaussianmix(data, means, variances, proportions, filename=None):
+    """
+    Plots a density histogram of the given data, and compares it with a gaussian mixture
+    with given parameters.
 
-r0, means, variances = model_params(F, p)
-print(r0)
-n, n_tries = 5000, 10
+    Input:
+        - data: histogram data to plot, of shape (n_features, n_plots)
+        - means: means of the gaussian mixtures, of size (n_gaussians, n_plots)
+        - variances: idem, for variances
+        - proportions: vector of probability for the gaussian mixtures
+        - filename: if given, saves plot to disk.
 
-tot = gen_data(F, p, r0=r0, n=n, n_tries=n_tries, datafile="data/histograms_two.txt")
-
-
-def plot_fluctuations(fname):
+    Output: none, but shows generated plot.
+    """
 
     from matplotlib import rc
     from matplotlib.ticker import FormatStrFormatter
@@ -121,21 +124,25 @@ def plot_fluctuations(fname):
     rc("text", usetex=True)
     rc("font", family="serif")
 
-    data = np.loadtxt(fname)
+    if len(data.shape) == 1:
+        data = data[:, None]
+        r0 = 1
+    else:
+        r0 = data.shape[1]
+
     colors = ["lightpink", "skyblue", "lightpink"]
     edgecolors = ["hotpink", "steelblue"]
-    fsize = 16
-    histnames = [f"Entries of $u_{i+1}$" for i in range(r0)]
 
     fig, ax = plt.subplots(
-        ncols=r0, sharey=True, gridspec_kw={"wspace": 0.1}, figsize=(11, 2)
+        ncols=r0,
+        sharey=True,
+        gridspec_kw={"wspace": 0.1},
+        figsize=(11, 2),
+        squeeze=False,
     )
 
     for i in range(r0):
-        ax[i].spines["right"].set_visible(False)
-        ax[i].spines["top"].set_visible(False)
-        ax[i].spines["left"].set_visible(False)
-        ax[i].hist(
+        ax[0, i].hist(
             data[:, i],
             bins=100,
             density=True,
@@ -144,82 +151,38 @@ def plot_fluctuations(fname):
             alpha=1,
             ec=edgecolors[i],
         )
-        # ax[i].set_title(histnames[i], fontsize=fsize)
-        ax[i].set_yticks([])
-        x_min, x_max = ax[i].get_xlim()
+        ax[0, i].set_yticks([])
+        x_min, x_max = ax[0, i].get_xlim()
         x = np.arange(x_min, x_max, 0.01)
         y_mat = (
             1
             / np.sqrt(2 * np.pi * variances[:, i])
             * np.exp(-np.square(x[:, np.newaxis] - means[:, i]) / (2 * variances[:, i]))
         )
-        y = y_mat.dot(p)
-        ax[i].plot(x, y, color="k", lw=2, alpha=0.7)
-        ax[i].xaxis.set_major_formatter(FormatStrFormatter("%0.1f"))
-    fsize2 = 16
+        y = y_mat.dot(proportions)
+        ax[0, i].plot(x, y, color="k", lw=2, alpha=0.7)
+        ax[0, i].xaxis.set_major_formatter(FormatStrFormatter("%0.1f"))
 
-    # ax[1].text(1,0,'$p_1\mathcal{N}(\mu_{2,1},\sigma_{2,1}^2)+p_2\mathcal{N}(\mu_{2,2},\sigma_{2,2}^2)$', fontsize=fsize2)
-
-    # ax[0].text(2,0.5,'$p_1\mathcal{N}(\mu_{i,1},\sigma_{i,1}^2)+p_2\mathcal{N}(\mu_{i,2},\sigma_{i,2}^2)$', fontsize=fsize2)
-    # ax[0].legend(loc=0, fontsize = fsize)
-    plt.savefig("pictures/fluctuations_two.pdf", bbox_inches="tight")
-    ax[0].set_zorder(100)
+    plt.savefig(filename, bbox_inches="tight")
+    ax[0, 0].set_zorder(100)
 
     plt.show()
 
 
-plot_fluctuations("data/histograms_two.txt")
+if __name__ == "__main__":
 
+    F1 = np.array([[6, 4], [5, 3]])
+    F2 = np.array([[48, 6], [12, 24]])
+    p = np.array((1 / 3, 2 / 3))
 
-def plot_fluctuations_one(fname):
+    for filename, F in zip(["histograms_one", "histograms_two"], [F1, F2]):
+        means, variances = gaussian_params(F, p)
 
-    from matplotlib import rc
-    from matplotlib.ticker import FormatStrFormatter
+        datafile = f"data/{filename}.txt"
+        if path.exists(datafile):
+            data = np.genfromtxt(datafile)
+        else:
+            n, n_tries = 5000, 20
+            data = gen_data(F, p, n_nodes=n, n_tries=n_tries, datafile=datafile)
 
-    rc("text", usetex=True)
-    rc("font", family="serif")
-
-    data = np.loadtxt(fname)
-    colors = ["lightpink", "skyblue", "lightpink"]
-    edgecolors = ["hotpink", "steelblue"]
-    fsize = 16
-    histnames = [f"Entries of $u_{i+1}$" for i in range(r0)]
-
-    fig, ax = plt.subplots(figsize=(11, 2))
-
-    # ax.spines['right'].set_visible(False)
-    # ax.spines['top'].set_visible(False)
-    # ax.spines['left'].set_visible(False)
-    ax.hist(
-        data,
-        bins=100,
-        density=True,
-        color=colors[0],
-        histtype="stepfilled",
-        alpha=1,
-        ec=edgecolors[0],
-    )
-    # ax.set_title(histnames[0], fontsize=fsize)
-    ax.set_yticks([])
-    x_min, x_max = ax.get_xlim()
-    x = np.arange(x_min, x_max, 0.01)
-    y_mat = (
-        1
-        / np.sqrt(2 * np.pi * variances[:, 0])
-        * np.exp(-np.square(x[:, np.newaxis] - means[:, 0]) / (2 * variances[:, 0]))
-    )
-    y = y_mat.dot(p)
-    ax.plot(x, y, color="k", lw=2, alpha=0.7)
-    ax.xaxis.set_major_formatter(FormatStrFormatter("%0.1f"))
-    fsize2 = 16
-
-    # ax[1].text(1,0,'$p_1\mathcal{N}(\mu_{2,1},\sigma_{2,1}^2)+p_2\mathcal{N}(\mu_{2,2},\sigma_{2,2}^2)$', fontsize=fsize2)
-
-    # ax[0].text(2,0.5,'$p_1\mathcal{N}(\mu_{i,1},\sigma_{i,1}^2)+p_2\mathcal{N}(\mu_{i,2},\sigma_{i,2}^2)$', fontsize=fsize2)
-    # ax[0].legend(loc=0, fontsize = fsize)
-    plt.savefig("pictures/fluctuations_sparse_one.pdf", bbox_inches="tight")
-
-    plt.show()
-
-
-# plot_fluctuations_one("data/histograms_one.txt")
+        plot_gaussianmix(data, means, variances, p, filename="pictures/{filename}.pdf")
